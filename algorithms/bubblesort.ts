d0bf08b8f93cdac4d2cbc89f8dceabaeece6bea5
Mandus <mandus.hogberg@gmail.com>
2025-02-21 01:39:54 +0100

    feat: bubblesort and insertion added

---
+++ algorithms/bubblesort.ts
@@ -0,0 +1,20 @@
+//Bubblesort sorts by comparing adjacent elements and swapping them if they are in the wrong order
+function bubblesort(arr: number[]): number[] {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let swapped = false;
+
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            return arr;
+        }
+    }
+    return arr;
+}
---
+++ algorithms/insertionsort.ts
@@ -0,0 +1,16 @@
+//Works by shifting elements to the right until the current element is greater than the element to its left
+function insertionsort(arr: number[]): number[] {
+    const n = arr.length;
+
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
